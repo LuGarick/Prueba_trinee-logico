3baa427b36ce8474d1f7e4f341081ac72330cac0
Selvin Najera <selvingts20@gmail.com>
2023-10-09 13:26:03 -0700

    first commit

---
+++ logic.js
@@ -0,0 +1,12 @@
+'use strict'
+let asientos = [10,10]
+let i = 0;
+let k = 1;
+
+for(i = 0; i <= 10; i++ ){
+    asientos[i,0] = " L "
+    document.write = asientos[i,0];
+    for(k = 1; k <=9; k++){
+        asientos[i, k] = " l "
+    }
+}
